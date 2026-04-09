247927deb93e76bfedf857b4abba6321f9ed6dad
Aditya Banerjee <aditya_banerjee@icloud.com>
2026-04-09 16:48:42 +0800

    add placeholder layout

---
+++ apps/website/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from "./header.astro";
+export { default as Footer } from "./footer.astro";
+export { default as Logo } from "./logo.astro";
+export { default as Navbar } from "./navbar.astro";
