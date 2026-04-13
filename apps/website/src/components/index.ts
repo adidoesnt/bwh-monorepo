29c6ab4acd407b796450e022f9a96efb190a90ff
Aditya Banerjee <aditya_banerjee@icloud.com>
2026-04-13 15:29:04 +0800

    add pillars section on hero page

--- apps/website/src/components/index.ts
+++ apps/website/src/components/index.ts
@@ -3,4 +3,5 @@ export { default as Footer } from "./footer.astro";
 export { default as FooterLinks } from "./footerLinks.svelte";
 export { default as Logo } from "./logo.astro";
 export { default as Navbar } from "./navbar.astro";
+export { default as PillarCards } from "./pillarCards.svelte";
 export { default as SearchBar } from "./searchBar.svelte";
---
+++ apps/website/src/components/pillarCards.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  const pillars = [
+    {
+      title: "learn",
+      subtitle: "blog",
+      href: "/blog",
+      img: {
+        src: "/pillars-learn.jpg",
+        alt: "learn",
+      },
+      description:
+        "learn about all things wellness, sustainability, and entrepreneurship!",
+    },
+    {
+      title: "train",
+      subtitle: "training",
+      href: "/training",
+      img: {
+        src: "/pillars-training.jpeg",
+        alt: "training",
+      },
+      description:
+        "explore our personal training packages to improve strength, conditioning, and mobility!",
+    },
+    {
+      title: "shop",
+      subtitle: "activewear",
+      href: "/activewear",
+      img: {
+        src: "/pillars-shop.jpg",
+        alt: "activewear",
+      },
+      description:
+        "join our waitlist for updates on our upcoming skin-safe activewear launch!",
+    },
+  ];
+</script>
+
+{#each pillars as pillar}
+  <a href={pillar.href}>
+    <div
+      class="bg-white flex h-full flex-col gap-4 rounded-sm p-4 shadow-md transition-all duration-300 hover:shadow-lg hover:scale-105"
+    >
+      <h2
+        class="font-headings text-dark-brown text-3xl font-bold tracking-wide"
+      >
+        {pillar.title}
+      </h2>
+      <div
+        class="flex aspect-video items-center justify-center overflow-hidden rounded-sm"
+      >
+        <img
+          src={pillar.img.src}
+          alt={pillar.img.alt}
+          class="h-full w-full object-cover"
+        />
+      </div>
+      <h3 class="font-body text-dark-brown text-2xl font-bold tracking-wide">
+        {pillar.subtitle}
+      </h3>
+      <hr class="border-dark-brown/60" />
+      <p class="font-body text-dark-brown text-base">
+        {pillar.description}
+      </p>
+    </div>
+  </a>
+{/each}
