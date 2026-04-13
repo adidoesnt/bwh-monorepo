c85dc6d02ffeaf0011b2102460add1be3b04c955
Aditya Banerjee <aditya_banerjee@icloud.com>
2026-04-13 16:04:58 +0800

    update footer font sizes

--- apps/website/src/components/footerLinks.svelte
+++ apps/website/src/components/footerLinks.svelte
@@ -5,13 +5,15 @@
     { href: "/blog", label: "Blog" },
     { href: "/training", label: "Training" },
     { href: "/activewear", label: "Activewear" },
-    { href: "/contact", label: "Contact Us" }
+    { href: "/contact", label: "Contact Us" },
   ];
 </script>
 
 <div class="flex flex-col">
-  <h3 class="font-headings mb-4 text-lg font-semibold">Quick Links</h3>
-  <ul class="font-body flex flex-col space-y-2 text-sm">
+  <h3 class="font-headings mb-4 text-lg font-semibold md:text-2xl">
+    Quick Links
+  </h3>
+  <ul class="font-body flex flex-col space-y-2 text-sm md:text-base">
     {#each links as route}
       <li class="flex items-center">
         <a href={route.href} class="hover:text-light-brown transition-colors">
